1c69e8113dc7197957c43900e5031ab472725c30
Pamela Ocampo <pamela.ocampo@gmail.com>
2014-11-27 22:55:46 -0500

    Add markers as API pages

--- js/index.js
+++ js/index.js
@@ -1,32 +1,36 @@
 var geoJson = [];
 var feed = new Instafeed({
-  get: 'user',
-  userId: 30792403,
-  accessToken: '30792403.467ede5.f5d03294259546698b7d513af731a00b',
-  useHttp: true,
-  template: '<h3>{{location}}</h3><a href="{{link}}"><img src="{{image}}" /></a><p>{{caption}}</p>',
-  filter: function(image) {
-      if(image.tags.indexOf('cafefront') >= 0){
-          geoJson.push({
-              type: 'Feature',
-              geometry: {
-                  type: 'Point',
-                  coordinates: [image.location.latitude, image.location.longitude]
-              },
-              properties: {
-                  description: image.caption.text,
-                  title: image.location.name,
-                  'marker-color': '#548cba'
-              }
-          });
-      }
-  },
-  after: function(){
-    if (this.hasNext()) {
-      feed.next();
+    get: 'user',
+    userId: 30792403,
+    accessToken: '30792403.467ede5.f5d03294259546698b7d513af731a00b',
+    useHttp: true,
+    template: '<a href="{{link}}" target="_blank"><img src="{{image}}"></a><p>{{image.caption.text.toString()}}</p>',
+    filter: function(image) {
+        if(image.tags.indexOf('cafefront') >= 0){
+            geoJson.push({
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [image.location.latitude,image.location.longitude]
+                },
+                "properties": {
+                    "title": image.location.name,
+                    "description": this.template,
+                    "marker-color": "#548cba",
+                        "marker-size": "large",
+                        "marker-symbol": "monument"
+                }
+            });
+        }
+    },
+    after: function(){
+        if (this.hasNext()) {
+            var paginatedLayer = L.mapbox.featureLayer();
+            paginatedLayer.setGeoJSON(geoJson);
+            geoJson = [];
+            paginatedLayer.addTo(map);
+            feed.next();
+        }
     }
-  },
-  success: function(){
-      photoLayer.setGeoJSON(geoJson);
-   }
 });
+feed.run();
