8218771ba2d37eb8933ba577bf07d0dc0998bea3
Pamela Ocampo <pamela.ocampo@gmail.com>
2014-11-27 23:56:11 -0500

    Custom marker pop up content

--- js/index.js
+++ js/index.js
@@ -7,18 +7,22 @@ var feed = new Instafeed({
     template: '<a href="{{link}}" target="_blank"><img src="{{image}}"></a><p>{{image.caption.text.toString()}}</p>',
     filter: function(image) {
         if(image.tags.indexOf('cafefront') >= 0){
+            var short_description = image.caption.text.split(' ').slice(2).join(' ');
+
             geoJson.push({
                 "type": "Feature",
                 "geometry": {
                     "type": "Point",
-                    "coordinates": [image.location.latitude,image.location.longitude]
+                    "coordinates": [image.location.longitude, image.location.latitude]
                 },
                 "properties": {
                     "title": image.location.name,
-                    "description": this.template,
+                    "image": image.images.low_resolution.url,
+                    "url": image.link,
+                    "description": short_description,
                     "marker-color": "#548cba",
-                        "marker-size": "large",
-                        "marker-symbol": "monument"
+                    "marker-size": "large",
+                    "marker-symbol": "cafe"
                 }
             });
         }
@@ -26,6 +30,19 @@ var feed = new Instafeed({
     after: function(){
         if (this.hasNext()) {
             var paginatedLayer = L.mapbox.featureLayer();
+            paginatedLayer.on('layeradd', function(e) {
+                var marker = e.layer,
+                feature = marker.feature;
+
+                var popupContent =  '<h3>' + feature.properties.title + '</h3>' +
+                    '<a target="_blank" class="popup" href="' + feature.properties.url + '">' +
+                    '<img src="' + feature.properties.image + '" />' +
+                    feature.properties.description + '</a>';
+
+                marker.bindPopup(popupContent, {
+                    minWidth: 320
+                });
+            });
             paginatedLayer.setGeoJSON(geoJson);
             geoJson = [];
             paginatedLayer.addTo(map);
